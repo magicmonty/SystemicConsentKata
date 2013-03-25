95f67f8952966299000495f71e9ad1ae083d5e7f
Martin Gondermann <magicmonty@pagansoft.de>
2013-03-25 20:26:34 +0100

    Initial import

---
+++ SystemicConsent/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SystemicConsent
+{
+    class MainClass
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine ("Hello World!");
+        }
+    }
+}
