9825a2799405a0dfec93bdb1416c8c04585effc8
Norman Mendel <No_Me@gmx.de>
2013-04-29 20:52:19 +0200

    Client Test

---
+++ Test/VoterClientTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test
+{
+    using NUnit.Framework;
+
+    public interface IOptionsProvider
+    {
+        Options GetOptionsForVote();
+    }
+
+
+
+    [TestFixture]
+    public class VoterClientTests
+    {
+        private IOptionsProvider optionsRepo;
+        private IVoterClient sut = null;
+
+        [SetUp]
+        public void Setup()
+        {
+           
+        }
+
+        [Test]
+        public void AnzeigeDerMoeglichenOptionen()
+        {
+            sut.StartVoting();
+            Options optionen = sut.GetOptions();
+            Assert.That(optionen, Is.Not.Null);
+
+        }
+    }
+}
